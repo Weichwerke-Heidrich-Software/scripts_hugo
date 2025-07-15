5e7a431b81c3559be5b46e1154788dd973f91d25
Simon Heidrich <info@wwh-soft.com>
2025-07-15 08:35:09 +0200

    Adding exceptions to missing translations.

--- missing_translations.sh
+++ missing_translations.sh
@@ -3,17 +3,21 @@
 set -e
 
 supported_translations=("de")
+exceptions=("privacy-policy" "refund-policy" "terms")
 
 cd "$(git rev-parse --show-toplevel)"
+cd content
 
 echo "= Checking for missing translations ="
 
-hugo --printI18nWarnings
-
-cd content
-
 count=0
 for file in $(find . -name "*.en.md"); do
+    for exception in "${exceptions[@]}"; do
+        if [[ "$file" == *"/$exception/index.en.md" ]]; then
+            echo "Skipping exception: $file"
+            continue 2
+        fi
+    done
     for translation in "${supported_translations[@]}";
     do
         if [ ! -f "${file%.en.md}.${translation}.md" ]; then
