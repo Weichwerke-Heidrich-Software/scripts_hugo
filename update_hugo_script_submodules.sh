7fdb2610ef85e54cff098a37caefdd4ab02f1a2c
Simon Heidrich <info@wwh-soft.com>
2025-07-18 07:46:58 +0200

    Script to update scripts_hugo submodules in all repos.

---
+++ update_hugo_script_submodules.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+for submod in ~/git/*/scripts_hugo/; do
+    cd "$submod"
+    git checkout main
+    git pull
+    cd ..
+
+    if [[ -n $(git status --porcelain) ]]; then
+        git add 
+        git commit -m "Updating submodule scripts_hugo."
+        git push
+    fi
+done
